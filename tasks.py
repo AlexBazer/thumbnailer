ad3ba47641099c9d1d3730dbe8e6ad7ac438d32a
Denis Gonsiorovsky <dns.gnsr@gmail.com>
2014-07-07 10:44:19 +0300

    Fixed: check for existing files

--- tasks.py
+++ tasks.py
@@ -1,10 +1,14 @@
 import urllib
 
 from celery import task
+import os
+
 
 @task
 def download_image(url, path):
     try:
+        if os.path.isfile(path):
+            return "EXISTS"
         urllib.urlretrieve(url, path)
     except Exception as exc:
         return "ERROR"
